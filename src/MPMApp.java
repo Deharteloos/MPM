a2bda5aaa78f180e065607c6cf1ef463af844380
Deharteloos <bbebwanga@gmail.com>
2019-05-18 22:18:15 +0100

    Everything is OK. Except the final node

--- src/MPMApp.java
+++ src/MPMApp.java
@@ -10,19 +10,17 @@ public class MPMApp {
         listeTaches.put("D", new TacheMPM("D", 5, "A,B"));
         listeTaches.put("E", new TacheMPM("E", 6, "C,D"));*/
 
-        listeTaches.put("A", new TacheMPM("A", 4));
-        listeTaches.put("B", new TacheMPM("B", 4, "D"));
-        listeTaches.put("C", new TacheMPM("C", 4, "A,D,B,H"));
-        listeTaches.put("D", new TacheMPM("D", 4, "A"));
-        listeTaches.put("E", new TacheMPM("E", 4, "A"));
-        listeTaches.put("F", new TacheMPM("F", 4, "A,D"));
-        listeTaches.put("G", new TacheMPM("G", 4, "D,F"));
-        listeTaches.put("H", new TacheMPM("H", 4, "A,E,I"));
-        listeTaches.put("I", new TacheMPM("I", 4, "A,D,E"));
+        listeTaches.put("A", new TacheMPM("A", 10));
+        listeTaches.put("B", new TacheMPM("B", 14, "D"));
+        listeTaches.put("C", new TacheMPM("C", 14, "A,D,B,H"));
+        listeTaches.put("D", new TacheMPM("D", 8, "A"));
+        listeTaches.put("E", new TacheMPM("E", 12, "A"));
+        listeTaches.put("F", new TacheMPM("F", 22, "A,D"));
+        listeTaches.put("G", new TacheMPM("G", 25, "D,F"));
+        listeTaches.put("H", new TacheMPM("H", 18, "A,E,I"));
+        listeTaches.put("I", new TacheMPM("I", 6, "A,D,E"));
 
-        Map<Integer, List<TacheMPM>> niv = MPM(listeTaches);
-
-        System.out.println("azertyuiop");
+        MPM(listeTaches);
     }
 
     public static Map<Integer, List<TacheMPM>> MPM(HashMap<String, TacheMPM> listeTaches) {
@@ -31,14 +29,15 @@ public class MPMApp {
         for(Map.Entry<String, TacheMPM> entry : listeTaches.entrySet()) {
             String key = entry.getKey();
             TacheMPM value = entry.getValue();
+
             if(value.getTachesAnt().size() == 0) ;
             else if(value.getTachesAnt().size() == 1) value.getTachesImmediatAnt().addAll(value.getTachesAnt());
             else{
                 Set<String> tmp = new HashSet<>();
                 List<String> taAnt = value.getTachesAnt();
+                Set<String> tmp_ = new HashSet<>();
                 for(String tache : taAnt)
                     tmp.addAll(listeTaches.get(tache).getTachesAnt());
-                Set<String> tmp_ = new HashSet<>();
                 while(tmp.size() != 0){
                     Iterator<String> it = tmp.iterator();
                     Set<String> temp = new HashSet<>();
@@ -56,11 +55,31 @@ public class MPMApp {
             }
         }
 
+        Map<String, TacheMPM> listetaches_ = new HashMap<>();
+        //listetaches_.putAll(listeTaches);
+        for(Map.Entry<String, TacheMPM> entry : listeTaches.entrySet()) {
+            String key = entry.getKey();
+            TacheMPM value = entry.getValue();
+            TacheMPM t = new TacheMPM();
+            t.setLast(value.isLast());
+            t.setNom(value.getNom());
+            t.setDuree(value.getDuree());
+            t.setDebutAuPlusTot(value.getDebutAuPlusTot());
+            t.setDebutAuPlusTard(value.getDebutAuPlusTard());
+            List<String> l = new ArrayList<>();
+            for(String s : value.getTachesImmediatAnt()){
+                String tmp = new String(s);
+                l.add(tmp);
+            }
+            t.setTachesImmediatAnt(l);
+            listetaches_.put(key, t);
+        }
+
         //Regroupement des tâches par niveau
         HashMap<String, TacheMPM> listeCopie = new HashMap<>();
         HashMap<String, TacheMPM> listeCopie_ = new HashMap<>();
-        listeCopie.putAll(listeTaches);
-        listeCopie_.putAll(listeTaches);
+        listeCopie.putAll(listetaches_);
+        listeCopie_.putAll(listetaches_);
         Map<Integer, List<TacheMPM>> niveaux = new HashMap<>();
         int i = 1, size_ = listeCopie.size();
         while(size_ != 0){
@@ -81,6 +100,92 @@ public class MPMApp {
             i++;
         }
 
+
+        //Ajout tache fin
+        TacheMPM fin = new TacheMPM("FIN");
+        List<TacheMPM> l = new ArrayList<>();
+        //Spécification des taches avant la tache de fin
+        List<String> stringList = new ArrayList<>();
+        for(TacheMPM tache : niveaux.get(new Integer(i-1))){
+            stringList.add(tache.getNom());
+            tache.setLast(true);
+        }
+        for(TacheMPM tache_ : niveaux.get(new Integer(i-2))){
+            tache_.setLast(true);
+            for(TacheMPM tache : niveaux.get(new Integer(i-1))){
+                if(tache.getTachesImmediatAnt().contains(tache_.getNom()))
+                    tache_.setLast(false);
+            }
+            if(tache_.isLast())
+                stringList.add(tache_.getNom());
+        }
+        fin.setTachesAnt(stringList);
+        fin.setTachesImmediatAnt(stringList);
+        l.add(fin);
+        niveaux.put(new Integer(i), l);
+
+        //Calcul des dates de début au plus tot et de début au plus tard
+        for(Map.Entry<Integer, List<TacheMPM>> entry : niveaux.entrySet()) {
+            Integer key = entry.getKey();
+            List<TacheMPM> value = entry.getValue();
+            for(TacheMPM tacheMPM : value){
+                tacheMPM.setDebutAuPlusTot(listeTaches);
+                listeTaches.put(tacheMPM.getNom(), tacheMPM);
+            }
+        }
+        for(int j = niveaux.size(); j > 0; j--){
+            List<TacheMPM> value = niveaux.get(new Integer(j));
+            for(TacheMPM tacheMPM : value){
+                tacheMPM.setDebutAuPlusTard(listeTaches);
+                listeTaches.put(tacheMPM.getNom(), tacheMPM);
+            }
+        }
+
+
+
+
+
+        /**
+         * Représentation du graphe par liste d'adjacences
+         * Chaque élément de la liste est représenté avec le formalisme suivant:
+         *  (nom_de_la_tache, durée, date_debut_plus_tot, date_debut_plus_tard)
+         */
+        System.out.print("(DEBUT, 0, 0, 0) -> [");
+        List<TacheMPM> taches = niveaux.get(1);
+        for(TacheMPM tch : taches){
+            afficherTache(tch);
+            System.out.print(" ; ");
+        }
+        System.out.println("]");
+        for(int a = 1; a < niveaux.size(); a++){
+            for(TacheMPM t : niveaux.get(a)){
+                afficherTache(t);
+                System.out.print(" -> [");
+                for(TacheMPM t_ : niveaux.get(a+1)){
+                    if(t_.getTachesImmediatAnt().contains(t.getNom())) {
+                        afficherTache(t_);
+                        System.out.print(" ; ");
+                    }
+                }
+                System.out.println("]");
+            }
+        }
+
+
+        //Affichage du chemin critique
+        System.out.println();
+        System.out.println();
+        System.out.println("----> Chemin critique <----");
+        System.out.print("DEBUT");
+        for(Map.Entry<String, TacheMPM> entry : listeTaches.entrySet()) {
+            String key = entry.getKey();
+            TacheMPM value = entry.getValue();
+            if(value.getDebutAuPlusTard() == value.getDebutAuPlusTot()){
+                System.out.print( " => ");
+                afficherTache(value);
+            }
+        }
+        System.out.println();
         return niveaux;
     }
 
@@ -93,6 +198,12 @@ public class MPMApp {
                 value.getTachesImmediatAnt().remove(nom);
             }
         }
+    }
+
+    public static void afficherTache(TacheMPM tache) {
+        System.out.print("(");
+        System.out.print(tache.getNom()+", "+tache.getDuree()+", "+tache.getDebutAuPlusTot()+", "+tache.getDebutAuPlusTard());
+        System.out.print(")");
     }
 
 }
--- src/TacheMPM.java
+++ src/TacheMPM.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class TacheMPM {
     private String nom;
@@ -9,6 +7,7 @@ public class TacheMPM {
     private int debutAuPlusTard;
     private List<String> tachesAnt = new ArrayList<>();
     private List<String> tachesImmediatAnt = new ArrayList<>();
+    private boolean last = false;
 
     public TacheMPM(){
 
@@ -27,6 +26,14 @@ public class TacheMPM {
         this.nom = nom;
         this.duree = duree;
         this.tachesAnt = Arrays.asList(tachesAnt.split(","));
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+
+    public void setLast(boolean last) {
+        this.last = last;
     }
 
     public String getNom() {
@@ -65,7 +72,62 @@ public class TacheMPM {
         return tachesAnt;
     }
 
+    public void setTachesAnt(List<String> tachesAnt) {
+        this.tachesAnt = tachesAnt;
+    }
+
+    public void setTachesImmediatAnt(List<String> tachesImmediatAnt) {
+        this.tachesImmediatAnt = tachesImmediatAnt;
+    }
+
     public List<String> getTachesImmediatAnt() {
         return tachesImmediatAnt;
+    }
+
+    public void setDebutAuPlusTot(HashMap<String, TacheMPM> listeTaches) {
+        int tmp;
+        if(tachesImmediatAnt.size()== 0)
+            this.debutAuPlusTot = 0;
+        else{
+            tmp = listeTaches.get(tachesImmediatAnt.get(0)).getDuree() + listeTaches.get(tachesImmediatAnt.get(0)).getDebutAuPlusTot();
+            for(String tache : tachesImmediatAnt){
+                int temp = listeTaches.get(tache).getDuree() + listeTaches.get(tache).getDebutAuPlusTot();
+                if(tmp < temp)
+                    tmp = temp;
+            }
+            this.debutAuPlusTot = tmp;
+        }
+    }
+
+    public void setDebutAuPlusTard(HashMap<String, TacheMPM> listeTaches) {
+        int tmp;
+        if(tachesImmediatAnt.size() == 0) {}
+        if(this.getNom().equals("FIN"))
+            this.debutAuPlusTard = this.debutAuPlusTot;
+        else {
+            List<TacheMPM> list = new ArrayList<>();
+            for(Map.Entry<String, TacheMPM> entry : listeTaches.entrySet()) {
+                String key = entry.getKey();
+                TacheMPM value = entry.getValue();
+                if(value.tachesImmediatAnt.contains(this.getNom()))
+                    list.add(value);
+            }
+            //Iterator<TacheMPM> it = list.iterator();
+            //int daptard = 0;
+            //if(it.hasNext()) daptard = it.next().getDebutAuPlusTard();
+            tmp = list.get(0).getDebutAuPlusTard() - this.duree;
+            for(TacheMPM tache : list){
+                int temp = tache.getDebutAuPlusTard() - this.duree;
+                if(tmp > temp)
+                    tmp = temp;
+            }
+            /*tmp = listeTaches.get(tachesImmediatAnt.get(0)).getDebutAuPlusTard() - listeTaches.get(tachesImmediatAnt.get(0)).getDebutAuPlusTot();
+            for(String tache : tachesImmediatAnt){
+                int temp = this.duree - listeTaches.get(tache).getDebutAuPlusTot();
+                if(tmp > temp)
+                    tmp = temp;
+            }*/
+            this.debutAuPlusTard = tmp;
+        }
     }
 }
